a4c1c2a3d1220174930159b68b1217ddea67c2a8
Gabin Gaby <gabindjomo21@gmail.com>
2023-01-24 20:39:44 +0100

    add animation

--- src/components/animatable/Divider.tsx
+++ src/components/animatable/Divider.tsx
@@ -1,35 +1,57 @@
-import React, { FC, memo } from 'react';
-import { StyleSheet } from 'react-native';
-import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+import React, {
+  FC,
+  memo,
+  useEffect
+} from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
 import { Height } from '../../utils/constants';
-import useTheme from '../../hooks/useTheme';
+import { useAtom } from 'jotai';
+import { textFocused } from '../../store/header/input';
+import { lightTheme } from '../../utils/constants/theme';
+
 
 type DividerProps = {}
 
 const Divider: FC<DividerProps> = ({ }) => {
-  const { value } = useTheme();
-  
-  const uas = useAnimatedStyle(() => ({
-    width: '100%',
-    backgroundColor: value.primary
+  const width = useSharedValue(0);
+  const [focused] = useAtom(textFocused);
 
+  const uas = useAnimatedStyle(() => ({
+    width: `${width.value}%`
   }));
 
+  useEffect(() => {
+    width.value = withTiming(focused ? 100 : 0);
+  }, [focused]);
+
   return (
-    <Animated.View
-      style={[
-        uas,
-        styles.divider
-      ]}
-    />
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          uas,
+          styles.divider
+        ]}
+      />
+    </View>
   )
 }
 
 export default memo<DividerProps>(Divider);
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    height: Height / 300,
+    backgroundColor :'transparent'
+  },
   divider: {
     height: Height / 300,
-    borderRadius: Height
+    backgroundColor: lightTheme.value.primary
   }
 })
--- src/screens/home/SearchHome.tsx
+++ src/screens/home/SearchHome.tsx
@@ -1,34 +1,40 @@
 import React, { FC } from 'react';
-import {
-  View,
-  TextInput,
-  Text
-} from '../../components/common';
+import { View, TextInput } from '../../components/common';
 import { ViewProps, StyleSheet } from 'react-native';
 import { Height, Width } from '../../utils/constants';
 import { IconButton } from '../../components/buttons';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import useTheme from '../../hooks/useTheme';
 import Divider from '../../components/animatable/Divider';
+import { useAtom } from 'jotai';
+import {
+  textFocused,
+  textInput
+} from '../../store/header/input';
 
-type SearchHomeProps = ViewProps & {
-
-}
+type SearchHomeProps = ViewProps & {}
 
 const SearchHome: FC<SearchHomeProps> = ({ ...props }) => {
   const { value } = useTheme();
+  const [text, setText] = useAtom(textInput);
+  const [, setTextFocused] = useAtom(textFocused);
+
+
   return (
-    <View
-      style={styles.container}
-      {...props}
-    >
-      <View style={styles.inputContainer}>
+    <View style={styles.container}  {...props}>
+      <View
+        style={{
+          ...styles.inputContainer,
+          backgroundColor: value.variant1,
+        }}
+      >
         <TextInput
+          value={text}
+          onChangeText={setText}
+          onFocus={() => setTextFocused(true)}
+          onBlur={() => setTextFocused(false)}
           placeholder="Entrer le titre ici..."
-          style={{
-            backgroundColor: value.variant1,
-            ...styles.input
-          }}
+          style={styles.input}
         />
         <Divider />
       </View>
@@ -41,11 +47,12 @@ const SearchHome: FC<SearchHomeProps> = ({ ...props }) => {
           <Ionicon
             name="add"
             size={Width / 12}
+            style={{ marginLeft: 5 }}
             color={value.background}
           />
         }
       />
-    </View>
+    </View >
   )
 }
 
@@ -63,11 +70,11 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexGrow: 1,
     borderRadius: Width / 5,
-    overflow : 'hidden',
-    alignItems:'center'
+    overflow: 'hidden',
+    alignItems: 'center'
   },
   input: {
-    width : '100%',
+    width: '100%',
     fontSize: Width / 20,
     paddingHorizontal: Width / 25,
     paddingVertical: Height / 60
